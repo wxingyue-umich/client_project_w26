17ce06e084c0d81c45fa0fbf8de34402bc6ced56
wxingyue-umich <wxingyue@umich.edu>
2026-03-29 23:00:54 -0400

    Unique Addition

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,11 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+document.querySelectorAll('.toggle-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        const card = btn.closest('.data, .library, .ux');
+        card.classList.toggle('show-back');
+
+        const isOpen = card.classList.contains('show-back');
+        btn.textContent = isOpen ? 'Back to overview' : 'Click to view course info';
+    });
+});
